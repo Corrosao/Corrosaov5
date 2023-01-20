260574ae0f13d282646c0f51b88af49bb7df91dc
Corrosao <redeblossom@gmail.com>
2023-01-19 22:43:27 -0300

    fix merge to save all files and create a txt file

--- LSM/merge.py
+++ LSM/merge.py
@@ -1,40 +1,69 @@
+import os
 import cv2
 import numpy as np
+import datetime
 
-# Load the images
-img1 = cv2.imread('/content/Corrosaov5/LSM/5_03_01_png.rf.1649f6b5935f5e1b870a7d5032ec8273.jpg')
-img2 = cv2.imread('/content/Corrosaov5/LSM/5_03_01_png.rf.1649f6b5935f5e1b870a7d5032ec8273(1).jpg')
+now = datetime.datetime.now()
 
-# calculate the mean squared error
-mse = ((img1 - img2) ** 2).mean()
-#mean squared error (MSE) or the mean absolute error (MAE) of the result.
-print(f'mean squared error: {mse}')
-mae = np.mean(np.abs(img1-img2))
-print(f'mean absolute error: {mae}')
-# merge the two images together with an opacity of 0.5
-merged_img = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+folder_name = f"lsm_{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}"
 
-# Create ORB object
-orb = cv2.ORB_create()
+# specify the path to the OUTPUT and OUTPUT_DETECT folders
+output_folder = '/content/Corrosaov5/output'
+output_detect_folder = '/content/Corrosaov5/output_detect'
 
-# Detect keypoints and compute descriptors for the first image
-kp1, des1 = orb.detectAndCompute(img1, None)
+# create a new folder
+save_folder = os.path.join('/content/Corrosaov5/LSM', folder_name)
+os.makedirs(save_folder)
 
-# Detect keypoints and compute descriptors for the second image
-kp2, des2 = orb.detectAndCompute(img2, None)
+# create a text file to save results
+txt_file = open(os.path.join(save_folder, 'results.txt'), 'w')
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+# get a list of all files in the OUTPUT folder
+output_files = os.listdir(output_folder)
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
+# get a list of all files in the OUTPUT_DETECT folder
+output_detect_files = os.listdir(output_detect_folder)
 
-# Draw matches
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# iterate through the files in the OUTPUT folder
+for file in output_files:
+    # construct the full file path
+    img1_path = os.path.join(output_folder, file)
+    # load the image
+    img1 = cv2.imread(img1_path)
+    # check if the file also exists in the OUTPUT_DETECT folder
+    if file in output_detect_files:
+        # construct the full file path
+        img2_path = os.path.join(output_detect_folder, file)
+        # load the image
+        img2 = cv2.imread(img2_path)
+        # calculate the mean squared error
+        mse = ((img1 - img2) ** 2).mean()
+        print(f'mean squared error for {file}: {mse}')
+        mae = np.mean(np.abs(img1-img2))
+        print(f'mean absolute error for {file}: {mae}')
+        # write the image name, mean squared error, and mean absolute error to the text file
+        txt_file.write(f'{file}\nmean squared error: {mse}\nmean absolute error: {mae}\n\n')
+        # merge the two images together with an opacity of 0.5
+        merged_img = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+        # Create ORB object
+        orb = cv2.ORB_create()
+        # Detect keypoints and compute descriptors for the first image
+        kp1, des1 = orb.detectAndCompute(img1, None)
+        # Detect keypoints and compute descriptors for the second image
+        kp2, des2 = orb.detectAndCompute(img2, None)
 
-cv2.imwrite('image3.jpg', img3)
-cv2.imwrite('image4.jpg',merged_img)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1,des2, k=2)
+        # Apply ratio test
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
+        # Draw matches
+        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        # save the images
+        # cv2.imwrite(os.path.join(save_folder,f'image3_{file}'), img3)
+        cv2.imwrite(os.path.join(save_folder,f'lsm_{file}'), merged_img)
+# close the text file
+txt_file.close()
